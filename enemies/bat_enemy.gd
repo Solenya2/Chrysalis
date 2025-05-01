1ff60144c3600b4e572671dadec7d496c88db8dc
Solenya silly <noahhereid@icloud.com>
2025-05-01 20:24:33 +0200

    conecting action

---
+++ enemies/bat_enemy.gd
@@ -0,0 +1,23 @@
+class_name BatEnemy
+extends Enemy
+
+@onready var navigation_agent_2d: NavigationAgent2D = %NavigationAgent2D
+@onready var chase_state: EnemyChaseState = (
+	EnemyChaseState.new()
+	.set_actor(self)
+	.set_navigation_agent(navigation_agent_2d)
+)
+@onready var knockback_state: = EnemyKnockbackState.new().set_actor(self)
+@onready var fsm: = FSM.new().set_state(chase_state)
+
+func _ready() -> void:
+	super()
+	hurtbox.hurt.connect(func(other_hitbox: Hitbox):
+		fsm.change_state(knockback_state.set_knockback(other_hitbox.knockback))
+		create_hit_particles(other_hitbox, load("res://effects/hit_particles.tscn"))
+		stats.health -= other_hitbox.damage
+	)
+	knockback_state.finished.connect(fsm.change_state.bind(chase_state))
+
+func _physics_process(delta: float) -> void:
+	fsm.state.physics_process(delta)
---
+++ hero/hero.gd
@@ -0,0 +1,90 @@
+class_name Hero
+extends CharacterBody2D
+
+const CUSTOM_RED: = Color("e64539")
+const SIDE_BIAS: = 0.1
+
+var stats: = ReferenceStash.hero_stats as Stats 
+
+@export var movement_stats: MovementStats
+@export var roll_movement_stats: MovementStats
+
+var direction: = Vector2.DOWN :
+	set(value):
+		if value == Vector2.ZERO: return
+		value = value.normalized()
+		direction = value
+		
+var facing_direction: = Vector2.DOWN :
+	set(value):
+		if value == Vector2.ZERO: return
+		value = value.normalized()
+		if abs(value.x) >= abs(value.y) - SIDE_BIAS:
+			value = Vector2(sign(value.x), 0)
+		else:
+			value = Vector2(0, sign(value.y))
+		facing_direction = value
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var flip_anchor: Node2D = $FlipAnchor
+@onready var sprite_2d: Sprite2D = $FlipAnchor/Sprite2D
+@onready var remote_transform_2d: RemoteTransform2D = $RemoteTransform2D
+@onready var flasher: Flasher = Flasher.new().set_target(sprite_2d).set_color(CUSTOM_RED)
+@onready var blinker: Blinker = Blinker.new().set_target(sprite_2d)
+@onready var hitbox: Hitbox = $FlipAnchor/Hitbox
+@onready var hurtbox: Hurtbox = $Hurtbox
+
+@onready var move_state: = HeroMoveState.new().set_actor(self) as HeroMoveState
+@onready var roll_state: = HeroRollState.new().set_actor(self)
+@onready var weapon_state: = HeroWeaponState.new().set_actor(self)
+@onready var fsm: = FSM.new().set_state(move_state)
+
+func _enter_tree() -> void:
+	MainInstances.hero = self
+
+func _exit_tree() -> void:
+	MainInstances.hero = null
+
+func _ready() -> void:
+	Events.request_camera_target.emit.call_deferred(remote_transform_2d)
+	hurtbox.hurt.connect(take_hit)
+	stats.no_health.connect(queue_free)
+	connect_action(move_state.request_roll, roll_state)
+	connect_action(move_state.request_weapon, weapon_state)
+	motion_mode = MOTION_MODE_FLOATING
+
+func _physics_process(delta: float) -> void:
+	fsm.state.physics_process(delta)
+
+func connect_action(action_signal: Signal, state: State) -> void:
+	if action_signal.is_connected(fsm.change_state):
+		action_signal.disconnect(fsm.change_state)
+	if state is not State: return
+	action_signal.connect(fsm.change_state.bind(state))
+	if not state.finished.is_connected(fsm.change_state):
+		state.finished.connect(fsm.change_state.bind(move_state))
+	state.set_actor(self)
+
+func take_hit(other_hitbox: Hitbox) -> void:
+	hurtbox.is_invincible = true
+	stats.health -= other_hitbox.damage
+	Events.request_camera_screenshake.emit(4, 0.3)
+	Events.hero_hurt.emit()
+	await flasher.flash(0.2)
+	await blinker.blink()
+	hurtbox.is_invincible = false
+
+func play_animation(animation: String) -> void:
+	var animation_name: = animation + "_" + get_direction_string()
+	animation_player.play(animation_name)
+
+func get_direction_string() -> String:
+	var direction_string: = ""
+	if facing_direction.x == 0.0:
+		if facing_direction.y < 0.0:
+			direction_string = "up"
+		else:
+			direction_string = "down"
+	else:
+		direction_string = "side"
+	return direction_string
